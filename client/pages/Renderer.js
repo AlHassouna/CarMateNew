dfcc939b828fdcda83d13f5fc7a428a16abc68a6
Hhasona <82958594+HassanHassouna@users.noreply.github.com>
2024-01-16 20:27:44 +0200

    admin panel (#14)

--- client/pages/Renderer.js
+++ client/pages/Renderer.js
@@ -1,91 +1,92 @@
 class Renderer {
-  /* ATTRIBUTES */
-  #partials = {};
-  #templates = {
-    navbar: null,
-    admin: null,
-    allServices: null,
-    addService: null,
-    serviceDetails: null,
-    part: null,
-    cart: null,
-  };
 
-  /* CONSTRUCTOR */
-  constructor() {
-    this.#compile();
-    this.#registerHelpers();
-    // this.#registerPartials();
-  }
+    /* ATTRIBUTES */
+    #partials = {};
+    #templates = {
+        navbar: null,
+        admin: null,
+        allServices: null,
+        addService: null,
+        serviceDetails: null,
+        part: null,
+        cart: null,
+    };
 
-  /* PRIVATE */
-  #setActiveNavItem() {}
-  #registerHelpers() {
-    Handlebars.registerHelper("multiply", (num1, num2) =>
-      (num1 * num2).toFixed(2)
-    );
-    Handlebars.registerHelper("divide", (num1, num2) =>
-      (num1 / num2).toFixed(5)
-    );
-  }
-  #registerPartials() {
-    for (const key of Object.keys(this.#partials)) {
-      $.ajax({
-        url: `../../templates/${key}.hbs`,
-        dataType: "html",
-        async: false,
-        success: (source) => {
-          this.#partials[key] = Handlebars.registerPartial(key, source);
-        },
-      });
+    /* CONSTRUCTOR */
+    constructor() {
+        this.#compile();
+        this.#registerHelpers();
+        // this.#registerPartials();
     }
-  }
 
-  /* PUBLIC API */
-  #compile() {
-    for (const key of Object.keys(this.#templates)) {
-      $.ajax({
-        url: `../../templates/${key}.hbs`,
-        dataType: "html",
-        async: false,
-        success: (source) => {
-          this.#templates[key] = Handlebars.compile(source);
-        },
-      });
+    /* PRIVATE */
+    #setActiveNavItem() {
     }
-  }
 
-  renderNavBar() {
-    $("body").append(this.#templates.navbar({}));
-  }
+    #registerHelpers() {
+        Handlebars.registerHelper("multiply", (num1, num2) =>
+            (num1 * num2).toFixed(2)
+        );
+        Handlebars.registerHelper("divide", (num1, num2) =>
+            (num1 / num2).toFixed(5)
+        );
+    }
 
-  renderAllServices(services) {
-    $("main").empty();
-    $("main").append(this.#templates.allServices({ services }));
-    // setActiveNavItem();
-  }
+    #registerPartials() {
+        for (const key of Object.keys(this.#partials)) {
+            $.ajax({
+                url: `../../templates/${key}.hbs`,
+                dataType: "html",
+                async: false,
+                success: (source) => {
+                    this.#partials[key] = Handlebars.registerPartial(key, source);
+                },
+            });
+        }
+    }
 
-  renderServiceDetails(service) {
-    $("main").empty();
-    $("main").append(this.#templates.serviceDetails(service));
-  }
+    /* PUBLIC API */
+    #compile() {
+        for (const key of Object.keys(this.#templates)) {
+            $.ajax({
+                url: `../../templates/${key}.hbs`,
+                dataType: "html",
+                async: false,
+                success: (source) => {
+                    this.#templates[key] = Handlebars.compile(source);
+                },
+            });
+        }
+    }
 
-  renderAddService(service) {
-    $("main").empty();
-    $("main").append(this.#templates.addService(service));
-  }
+    renderNavBar() {
+        $("body").append(this.#templates.navbar({}));
+    }
 
-  renderAllUsers(users) {
-    $("main").empty();
-    $("main").append(this.#templates.admin({ users }));
-  }
 
-  renderAllPart(parts) {
-    $("main").empty();
-    $("main").append(this.#templates.part({ parts }));
-  }
-  renderAllCart(carts) {
-    $("main").empty();
-    $("main").append(this.#templates.cart({ carts }));
-  }
+    renderAllServices(services) {
+        $("main").empty();
+        $("main").append(this.#templates.allServices({services}));
+        // setActiveNavItem();
+    }
+
+    renderServiceDetails(service) {
+        $("main").empty();
+        $("main").append(this.#templates.serviceDetails(service));
+    }
+
+    renderAllUsers(users) {
+        $("main").empty();
+        $("main").append(this.#templates.admin(users));
+    }
+
+    renderAllPart(parts) {
+        $("main").empty();
+        $("main").append(this.#templates.part({parts}));
+    }
+
+    renderAllCart(carts) {
+        $("main").empty();
+        $("main").append(this.#templates.cart({carts}));
+    }
 }
