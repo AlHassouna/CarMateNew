3158f1e574c4d6f31333ace15384d99b67618961
Muaz Abdin <muathabd25@gmail.com>
2024-01-16 11:00:05 +0200

    update FrontEnd structre file

--- client/pages/APIManager.js
+++ client/pages/APIManager.js
@@ -8,6 +8,25 @@ class APIManager {
   /* PRIVATE */
 
   /* PUBLIC API */
+  getAllServices() {
+    $.ajax({
+      url: "../../assets/services.json",
+      async: false,
+      success: (services) => {
+        this.#data = services;
+      },
+    });
+  }
+
+  getAllUsers() {
+    $.ajax({
+      url: "../../assets/users.json",
+      async: false,
+      success: (users) => {
+        this.#data = users;
+      },
+    });
+  }
 
   get data() {
     return this.#data;
--- client/pages/main.js
+++ client/pages/main.js
@@ -1,2 +1,13 @@
-const model = new Model();
+const apiManager = new APIManager();
 const renderer = new Renderer();
+renderer.renderNavBar();
+
+function showAllServices() {
+  apiManager.getAllServices();
+  renderer.renderAllServices(apiManager.data);
+}
+
+function showAllUsers() {
+  apiManager.getAllUsers();
+  renderer.renderAllUsers(apiManager.data);
+}
