32dad9319b49c116511a9ad165d233c5ab6eac7b
lyaleaz <lyale000az@gmail.com>
2024-01-17 14:24:11 +0200

    filter feater

--- client/pages/main.js
+++ client/pages/main.js
@@ -27,3 +27,38 @@ function AddToCart() {
 function showAllCart() {
   renderer.renderAllCart(renderer.getCartData());
 }
+function searchByPartName() {
+  var searchInput = document.getElementById("searchInput").value.toLowerCase();
+  var parts = document.getElementsByClassName("part-minicard");
+
+  for (var i = 0; i < parts.length; i++) {
+    var partName = parts[i].querySelector("h2").textContent.toLowerCase();
+
+    if (partName.includes(searchInput)) {
+      parts[i].style.display = "block";
+    } else {
+      parts[i].style.display = "none";
+    }
+  }
+}
+function filterByCategory() {
+  var selectedCategory = document.getElementById("categoryFilter").value;
+  var parts = document.getElementsByClassName("part-minicard");
+
+  for (var i = 0; i < parts.length; i++) {
+    var category = parts[i].getAttribute("data-category");
+
+    if (selectedCategory === "" || selectedCategory === category) {
+      parts[i].style.display = "block";
+    } else {
+      parts[i].style.display = "none";
+    }
+  }
+}
+
+function resetCategoryFilter() {
+  var categoryFilter = document.getElementById("categoryFilter");
+  categoryFilter.selectedIndex = -1;
+  filterByCategory();
+  searchByPartName();
+}
