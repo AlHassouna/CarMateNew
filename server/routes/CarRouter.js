3bacc19426fc05bb2317cacdcda951ad71a1f892
lyaleaz <lyale000az@gmail.com>
2024-01-17 16:24:55 +0200

    fix CSS

--- server/routes/CarRouter.js
+++ server/routes/CarRouter.js
@@ -1,21 +1,20 @@
-import BaseRouter from './BaseRouter.js';
-import Car from '../models/Car.js';
+import BaseRouter from "./BaseRouter.js";
+import Car from "../models/Car.js";
 
 class CarRouter extends BaseRouter {
-    constructor() {
-        super(Car);
-    }
+  constructor() {
+    super(Car);
+  }
 
-    async getAll(req, res) {
-        try {
-            const cars = await this.model.find({});
-            res.send(cars);
-        } catch (error) {
-            console.log(error);
-            res.status(500).send('Internal Server Error');
-        }
+  async getAll(req, res) {
+    try {
+      const cars = await this.model.find({});
+      res.send(cars);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("Internal Server Error");
     }
-
+  }
 }
 
 export default new CarRouter().router;
