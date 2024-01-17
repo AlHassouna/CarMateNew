5fd052855d0b5172d072931fc4c06c4945cd21d5
HassanHassouna <hhasona@gmail.com>
2024-01-17 10:45:15 +0200

    adding user router

--- server/routes/garageApi.js
+++ server/routes/garageApi.js
@@ -3,8 +3,10 @@ import CarRouter from "./CarRouter.js";
 import ServiceRouter from "./ServiceRouter.js";
 import PartRouter from "./PartRouter.js";
 import OrderRouter from "./OrderRouter.js";
+import UserRouter from "./UserRouter.js";
 
 const router = express.Router();
+router.use('/users', UserRouter);
 router.use('/cars', CarRouter);
 router.use('/services', ServiceRouter);
 router.use('/parts', PartRouter);
