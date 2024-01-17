efa2319886ae1e387cf90e0fce25e0757136ed1b
Hhasona <82958594+HassanHassouna@users.noreply.github.com>
2024-01-17 10:53:18 +0200

    restructure the backend (#18)
    * restructure the backend
    
    * adding user router

--- server/routes/garageApi.js
+++ server/routes/garageApi.js
@@ -1,86 +1,15 @@
 import express from "express";
-import Car from "../models/Car.js";
-import Service from "../models/Service.js";
-import Part from "../models/Part.js";
-import Order from "../models/Order.js";
-import User from "../models/User.js";
+import CarRouter from "./CarRouter.js";
+import ServiceRouter from "./ServiceRouter.js";
+import PartRouter from "./PartRouter.js";
+import OrderRouter from "./OrderRouter.js";
+import UserRouter from "./UserRouter.js";
 
 const router = express.Router();
-
-router.get("/cars", async (req, res) => {
-  try {
-    const car = await Car.find({});
-    res.send(car);
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-router.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-router.get("/services", async (req, res) => {
-  try {
-    const services = await Service.find({});
-    res.send(services);
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-router.post("/services", async (req, res) => {
-  const service = req.body;
-  try {
-    const newService = await Service.create(service);
-    const services = await Service.find({});
-    res.send(services);
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-router.get("/parts", async (req, res) => {
-  try {
-    const parts = await Part.find({});
-    res.send(parts);
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-router.get("/orders", async (req, res) => {
-  try {
-    const parts = await Order.find({});
-    res.send(parts);
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-// router.get("/cart", async (req, res) => {
-//   try {
-//     const cart = await CartModel.find({});
-//     res.send(cart);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
-
-// router.post("/cart", async (req, res) => {
-//   const cart = req.body;
-//   try {
-//     const newCart = await CartModel.create(cart);
-//     const cart = await CartModel.find({});
-//     res.send(cart);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
+router.use('/users', UserRouter);
+router.use('/cars', CarRouter);
+router.use('/services', ServiceRouter);
+router.use('/parts', PartRouter);
+router.use('/orders', OrderRouter);
 
 export default router;
